4aea3602456a5dec0caed36e81e4eb985ac81d55
51133113 <51133113@nfu.edu.tw>
2023-04-14 11:59:04 +0800

    6

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'Vocabulary', 'text': '/downloads/cambridge_english_vocabulary_list.pdf \n /downloads/The_Oxford_5000.pdf \n', 'tags': '', 'url': 'Vocabulary.html'}, {'title': 'About', 'text': '我是白癡8787', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'Vocabulary', 'text': '/downloads/cambridge_english_vocabulary_list.pdf \n /downloads/The_Oxford_5000.pdf \n \n', 'tags': '', 'url': 'Vocabulary.html'}, {'title': 'About', 'text': '我是白癡8787', 'tags': '', 'url': 'About.html'}]};
